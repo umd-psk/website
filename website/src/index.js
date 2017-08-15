9be12df7ecf9d4ba2d1bd25d118de7ea526c1465
jwcorle <JCorley111@gmail.com>
2017-08-15 14:02:35 -0400

    Initial structure refactor.

--- website/src/index.js
+++ website/src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
+import MainNavbar from './components/MainNavbar';
 import registerServiceWorker from './registerServiceWorker';
+import 'bootstrap-css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<MainNavbar />, document.getElementById('root'));
 registerServiceWorker();
