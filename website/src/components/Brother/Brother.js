d6fde804864dda7dbdf9b54c97e836892432d059
jwcorle <JCorley111@gmail.com>
2017-08-18 13:26:25 -0400

    Pictures and format slight adjustment.

--- website/src/components/Brother/Brother.js
+++ website/src/components/Brother/Brother.js
@@ -1,13 +1,24 @@
 import React from 'react'
 import './Brother.css'
 
-export default class Brother {
+const brotherFolder = require.context('./brothers', false);
+
+export default class Brother extends React.Component {
   render() {
+    let src = 'blank.png'
+    try {
+      src = brotherFolder('./' + this.props._name + '.jpg', true);
+    } catch(e) {
+      src = brotherFolder('./blank.png', true);
+    }
+
     return (
-      <div>
-        <img src={"./brothers/${this.props.name}.jpg"} />
-        <h3>{this.props.name}</h3>
-        <h4>{this.props.position}</h4>
+      <div className='brother'>
+        <div className="crop">
+          <img src={src} alt='' />
+        </div>
+        <h3>{this.props._name}</h3>
+        <h5>{this.props.chair}</h5>
       </div>
     );
   }
--- website/src/routes/BrothersPage/BrothersPage.js
+++ website/src/routes/BrothersPage/BrothersPage.js
@@ -1,6 +1,66 @@
 import React from 'react'
 import { Container, Row, Col } from 'reactstrap'
 import App from '../../components/App'
+import Brother from '../../components/Brother'
+
+var allBrothers = {
+  "Santiago Noguera": "",
+  "Paul Hazan": "",
+  "Jesse Bengel": "",
+  "Sebastian Lecha": "Senior Chair",
+  "Hugo Caldwell": "External Philathropy Chair",
+  "Michael Cassano": "",
+  "Zachary Goldman": "",
+  "Brian Tran-Dac": "Greek Week Chair",
+  "Ryan Connors": "",
+  "Anthony Starego": "",
+  "Peter Danis": "Public Relations Chair",
+  "Elias Yishak": "",
+  "Jared McKee": "Public Relations Chair",
+  "Kurt Barsotti": "TerpThon Chair",
+  "Mark Bedanov-Pack": "Scholarship Chair",
+  "Zachary Lessner": "",
+  "Perry Ganz": "Sentinel",
+  "Max Cislo": "Athletics Chair",
+  "Joseph Hathway": "Brotherhood Chair",
+  "Michael Kelleher": "President",
+  "Connor Bell": "Away Weekend Chair",
+  "Zachary Yale": "House Manager",
+  "Tyler Weiss": "Treasurer",
+  "Gunnar Terjesen": "Away Weekend Chair",
+  "Alex Littlehales": "",
+  "Harry Culleton": "Social Chair",
+  "Colin Wagner": "",
+  "Samuel Lupo": "",
+  "Matt Labib": "",
+  "Joshua Schnitzer": "Vice President",
+  "Brian McKeon": "Rush Chair",
+  "Nicholas Garibaldi": "Inductor",
+  "Ryan Boyd": "Rush Chair",
+  "Joseph Cipolla": "",
+  "Brandon Segal": "",
+  "Brandon Kao": "Secretary",
+  "Benjamin Eapen": "Social Chair",
+  "Shawn Glabach": "",
+  "Michael Berhane": "Diversity Chair",
+  "Harrison Kraus": "\"Lit\" Chair",
+  "Jordan Riese": "",
+  "Robert Lewis": "Fundraising Chair",
+  "Jacob Chavis": "Website Chair",
+  "James Corley": "Rush Chair",
+  "Liam Fulling": "",
+  "Benjamin Fleury V": "",
+  "David Klotz": "",
+  "Justin Frankle": ""
+};
+
+const brothers = Object.keys(allBrothers).map(function(name, id) {
+  return (
+    <Col md="4" key={id}>
+      <Brother chair={allBrothers[name]} _name={name} />
+    </Col>
+  );
+});
 
 export default class BrothersPage extends React.Component {
   render() {
@@ -8,8 +68,8 @@ export default class BrothersPage extends React.Component {
       <App>
         <Container>
           <h1 className="pageTitle" data-aos="fade-down"> Brothers </h1>
-          <Row>
-            <Col> </Col>
+          <Row className="text-center">
+            {brothers}
           </Row>
         </Container>
       </App>
