07d5445772d32fe2d68047c46a1ec551b6603b1b
jwcorle <JCorley111@gmail.com>
2017-08-18 11:16:20 -0400

    Adjusted animations on Homepage.

--- website/src/routes/BrothersPage/BrothersPage.js
+++ website/src/routes/BrothersPage/BrothersPage.js
@@ -1,11 +1,14 @@
 import React from 'react'
+import App from '../../components/App'
 
 export default class BrothersPage extends React.Component {
   render() {
     return (
-      <div>
-        <p> Brothers Page </p>
-      </div>
-    )
+      <App>
+        <Container>
+          <h1 className="pageTitle" data-aos="fade-down"> Brothers </h1>
+        </Container>
+      </App>
+    );
   }
 }
--- website/src/routes/HomePage/HomePage.js
+++ website/src/routes/HomePage/HomePage.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import 'animate.css'
 import App from '../../components/App'
 import './HomePage.css'
 import logo from './ETA-logo.svg'
@@ -11,10 +10,10 @@ export default class Home extends React.Component {
         <App HomePage>
           <div className='bg-img'></div>
           <div className='titlePage'>
-              <img src={logo} alt='' className='animated fadeInDown delay-03s'/>
-              <h1 className='animated fadeInDown delay-04s'>Phi Sigma Kappa</h1>
-            <h3 className='animated fadeInUp delay-05s'> <b>Brotherhood.</b> Scholarship. <b>Character.</b> </h3>
-            <h4 className='animated fadeInUp delay-06s'> University of Maryland </h4>
+              <img src={logo} alt='' data-aos='fade-down' data-aos-delay="100"/>
+              <h1 data-aos='fade-down' data-aos-delay="400">Phi Sigma Kappa</h1>
+            <h3 data-aos='fade-up' data-aos-delay="400"> <b>Brotherhood.</b> Scholarship. <b>Character.</b> </h3>
+            <h4 data-aos='fade-up' data-aos-delay="650"> University of Maryland </h4>
           </div>
         </App>
       </div>
