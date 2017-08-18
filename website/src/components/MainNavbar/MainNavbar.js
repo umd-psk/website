f9a62296d9d24a55d0d27deaee69edf79b66c02c
jwcorle <JCorley111@gmail.com>
2017-08-17 22:14:48 -0400

    Navbar properly configured, fully responsive.

---
+++ website/src/components/MainNavbar/MainNavbar.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
+import WOW from 'wowjs'
+
+import logo from './logo.svg'
+import './UINav.css'
+
+export default class MainNavbar extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <Navbar color="faded" light toggleable="sm">
+          <NavbarToggler right onClick={this.toggle} />
+          <NavbarBrand className="abs" href="/">Phi Sigma Kappa</NavbarBrand>
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav navbar>
+              <NavItem>
+                <NavLink href="/">Home</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/">About</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/">Brothers</NavLink>
+              </NavItem>
+            </Nav>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <NavLink href="/">Social</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/">Awards</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/">Log In</NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
+    );
+  }
+}
