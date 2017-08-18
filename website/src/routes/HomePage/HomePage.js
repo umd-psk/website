5a3a6b0adeeea2078452142aea665754779bc3dd
jwcorle <JCorley111@gmail.com>
2017-08-18 00:27:09 -0400

    Styling and images added.

---
+++ website/src/routes/HomePage/HomePage.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import App from '../../components/App'
+import './HomePage.css'
+import logo from './ETA-logo.svg'
+
+export default class Home extends React.Component {
+  render() {
+    return (
+      <div>
+        <App HomePage>
+          <div className='bg-img'></div>
+          <div className='container'>
+            <img src={logo} alt='' />
+            <h1 className='animated fadeInDown delay-07s'>Phi Sigma Kappa</h1>
+                <h3> Brotherhood. Scholarship. Character. </h3>
+                <h4> University of Maryland </h4>
+          </div>
+        </App>
+      </div>
+    )
+  }
+}
