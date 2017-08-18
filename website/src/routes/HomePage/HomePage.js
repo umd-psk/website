26b57079933886f233233b2d43277cbdfd8f85f1
jwcorle <JCorley111@gmail.com>
2017-08-18 02:20:23 -0400

    A couple animations.

--- website/src/routes/HomePage/HomePage.js
+++ website/src/routes/HomePage/HomePage.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import 'animate.css'
 import App from '../../components/App'
 import './HomePage.css'
 import logo from './ETA-logo.svg'
@@ -10,10 +11,10 @@ export default class Home extends React.Component {
         <App HomePage>
           <div className='bg-img'></div>
           <div className='titlePage'>
-            <img src={logo} alt='' />
-            <h1 className='animated fadeInDown delay-07s'>Phi Sigma Kappa</h1>
-                <h3> <b>Brotherhood.</b> Scholarship. <b>Character.</b> </h3>
-                <h4> University of Maryland </h4>
+              <img src={logo} alt='' className='animated fadeInDown delay-03s'/>
+              <h1 className='animated fadeInDown delay-04s'>Phi Sigma Kappa</h1>
+            <h3 className='animated fadeInUp delay-05s'> <b>Brotherhood.</b> Scholarship. <b>Character.</b> </h3>
+            <h4 className='animated fadeInUp delay-06s'> University of Maryland </h4>
           </div>
         </App>
       </div>
