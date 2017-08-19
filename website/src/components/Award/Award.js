a5e11139d19af737edbfe6a5a5dd6ad81c52c1da
jwcorle <JCorley111@gmail.com>
2017-08-19 16:08:58 -0400

    AwardsPage fully implemented.

--- website/src/components/Award/Award.js
+++ website/src/components/Award/Award.js
@@ -1,18 +1,18 @@
 import React from 'react'
-import { Container, Row, Col } from 'reactstrap'
+import { Col } from 'reactstrap'
 import './Award.css'
 
 export default class Award extends React.Component {
   render() {
-    let classes = 'award z-depth-2 '
+    let classes = 'award z-depth-3 '
     classes += (this.props.alt) ? 'alt' : '';
 
     return (
-      <Col md="4" className={classes}>
-        <div className="awardWrapper">
-          <h1> 2017 </h1>
-          <h2> Chapter of the Year </h2>
-          <p> Information about the award might go here. </p>
+      <Col md="4" className="awardCol">
+        <div className={classes}>
+          <h1> {this.props.year} </h1>
+          <h2> {this.props.awardName} </h2>
+          <p> {this.props.info} </p>
         </div>
       </Col>
     );
