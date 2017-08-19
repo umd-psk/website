b132838982991eaadd2235f8953ad626d27005b1
jwcorle <JCorley111@gmail.com>
2017-08-18 20:32:02 -0400

    Added SocialPage component.

---
+++ website/src/routes/SocialPage/SocialPage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Container, Row } from 'reactstrap'
+import App from '../../components/App'
+
+const SocialPage = () => (
+  <App>
+    <Container>
+      <h1 className='pageTitle' data-aos='fade-down'> Social Media </h1>
+      <p> Social Page </p>
+    </Container>
+  </App>
+);
+
+export default SocialPage;
