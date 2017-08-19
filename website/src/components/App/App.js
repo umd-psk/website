287217f6ea9df442f8f63ffc2645fe9f7c509418
jwcorle <JCorley111@gmail.com>
2017-08-19 12:18:37 -0400

    Removed need for social page.

--- website/src/components/App/App.js
+++ website/src/components/App/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from '../UINav'
 import AOS from 'aos'
 import 'aos/dist/aos.css';
+import 'font-awesome/css/font-awesome.css'
 import './App.css'
 
 export default class App extends React.Component {
--- website/src/routes/SocialPage/SocialPage.js
+++ website/src/routes/SocialPage/SocialPage.js
@@ -6,7 +6,7 @@ const SocialPage = () => (
   <App>
     <Container>
       <h1 className='pageTitle' data-aos='fade-down'> Social Media </h1>
-      <p> Social Page </p>
+      <p> <span className="fa fa-facebook fa-5x"></span> </p>
     </Container>
   </App>
 );
