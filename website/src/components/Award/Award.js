207ec28955070191dae5db78490238babf142d3e
jwcorle <JCorley111@gmail.com>
2017-08-19 15:41:15 -0400

    Awards Component update.

--- website/src/components/Award/Award.js
+++ website/src/components/Award/Award.js
@@ -4,32 +4,17 @@ import './Award.css'
 
 export default class Award extends React.Component {
   render() {
-    let classes = 'event z-depth-2 '
+    let classes = 'award z-depth-2 '
     classes += (this.props.alt) ? 'alt' : '';
 
     return (
-      <Container fluid className={classes}>
-        <Row>
-          <Col md="4" className="eventDate">
-            <div className="dateWrapper">
-              <h1> {this.props.date} </h1>
-              <h2> {this.props.month} </h2>
-              <h5> {this.props.time} </h5>
-            </div>
-          </Col>
-
-          <Col md="8" className="eventDetails">
-            <h2> {this.props.eventName} </h2>
-            {this.props.location &&
-                <h4>
-                  <i className="fa fa-map-marker"></i>
-                  {this.props.location}
-                </h4>
-            }
-            {this.props.children}
-          </Col>
-        </Row>
-      </Container>
+      <Col md="4" className={classes}>
+        <div className="awardWrapper">
+          <h1> 2017 </h1>
+          <h2> Chapter of the Year </h2>
+          <p> Information about the award might go here. </p>
+        </div>
+      </Col>
     );
   }
 }
