676852587274327d5a9a2ac7b6fa8d1d1aad5b2f
jwcorle <JCorley111@gmail.com>
2017-08-27 15:43:27 -0400

    Added rush events.

--- frontend/src/routes/RushPage/RushPage.js
+++ frontend/src/routes/RushPage/RushPage.js
@@ -11,13 +11,15 @@ const RushPage = () => (
       <h2 className='pageSubtitle' data-aos='fade-down' data-aos-delay='100'> Fall 2017 </h2>
 
       <Event date={'26'} month={'Aug'} eventName={'Move in Day'} />
-      <Event date={'6'} month={'Sep'} time={'12:00 PM - 4:00 PM'} eventName={'Meet the Greeks'} location={'McKeldin Mall'} alt />
-      <Event date={'7'} month={'Sep'} time={'7:00 PM - 8:00 PM'} eventName={'Chapter Open House'} location={'2 Fraternity Row'}>
-        <p> See our house on the row and meet more of our brothers. Pizza will be provided. </p>
+      <Event date={'6'} month={'Sep'} eventName={'Meet the Greeks'} location={'McKeldin Mall'} alt />
+      <Event date={'7'} month={'Sep'} time={'6:00 PM - 8:00 PM'} eventName={'Chapter Open House'} location={'2 Fraternity Row'}>
+        <p> See our house on the row and meet our brothers. Chipotle will be catered. </p>
       </Event>
-      <Event date={'9'} month={'Sep'} time={'12:00 PM - 3:00 PM'} eventName={'Burgers and Balling'} location={'2 Fraternity Row'} alt>
+      <Event date={'9'} month={'Sep'} time={'12:00 PM - 4:00 PM'} eventName={'Burgers and Ballin\''} location={'2 Fraternity Row'} alt>
         <p> Come out and ball out with our brothers in the court next to Phi Sigma Kappa's house. We will be grilling burgers as well. </p>
       </Event>
+      <Event date={'11'} month={'Sep'} time={'6:00 PM - 9:00 PM'} eventName={'Fifa Tournament'} location={'Invite Only'} />
+      <Event date={'13'} month={'Sep'} time={'6:00 PM - 8:00 PM'} eventName={'Cornerstone Wing Night'} location={'Invite Only'} alt />
     </Container>
   </App>
 );
