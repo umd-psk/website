0bbb9afcac051658aa087cae38eb38f854c57249
phomann13 <phomann1234@gmail.com>
2023-06-02 23:30:30 -0400

    Chairs and Awards

--- src/routes/AwardsPage/AwardsPage.js
+++ src/routes/AwardsPage/AwardsPage.js
@@ -11,19 +11,22 @@ export default class AwardsPage extends React.Component {
         <Container fluid>
           <h1 className='pageTitle' data-aos='fade-down'> Awards </h1>
           <Row className='awards'>
-            <Award year={'2019'} awardName={'Best Alumni Award'} />
-            <Award year={'2018'} awardName={'Best Alumni Award'} alt />
-            <Award year={'2017'} awardName={'Technology Award'} />
+            <Award className = 'award' year={'2023'} awardName={'Outstanding Mental Health Programming'} alt/>
+            <Award className = 'award' year={'2023'} awardName={'Most Improved Chapter'} />
+            <Award className = 'award' year={'2019'} awardName={'Best Alumni Award'}alt />
+            <Award year={'2018'} awardName={'Best Alumni Award'}  />
+            <Award year={'2017'} awardName={'Technology Award'} alt/>
             <Award year={'2017'} awardName={'Best Alumni Award'} />
             <Award year={'2016'} awardName={'Chapter of the Year'} alt />
             <Award year={'2016'} awardName={'Best Alumni Award'} />
-            <Award year={'2015'} awardName={'Most Improved Chapter'} />
+            <Award year={'2015'} awardName={'Most Improved Chapter'}alt />
             <Award year={'2015'} awardName={'Best Alumni Award'} />
-            <Award year={'2014'} awardName={'Best Alumni Award'} />
-            <Award year={'2013'} awardName={'Chapter of the Year'} alt />
-            <Award year={'2013'} awardName={'Best Alumni Award'} />
+            <Award year={'2014'} awardName={'Best Alumni Award'} alt/>
+            <Award year={'2013'} awardName={'Chapter of the Year'}  />
+            <Award year={'2013'} awardName={'Best Alumni Award'} alt/>
             <Award year={'2012'} awardName={'Technology Award'} />
-            <Award year={'2012'} awardName={'Athletics Award'} />
+            <Award year={'2012'} awardName={'Athletics Award'}alt />
+            
           </Row>
         </Container>
       </App>
