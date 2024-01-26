908340d14517e31422f0a2e584e85d9887a86d9a
phomann13 <phomann1234@gmail.com>
2024-01-26 01:55:56 -0500

    Rush Updates to calendar

--- src/routes/RushPage/RushPage.js
+++ src/routes/RushPage/RushPage.js
@@ -17,7 +17,7 @@ const RushPage = () => (
       <h1 className='pageTitle' data-aos='fade-down'> Rush </h1>
       <h2 className='pageSubtitle' data-aos='fade-down' data-aos-delay='100'>{currentSemester()}</h2>
 
-         <h4 className='secondaryText' data-aos='fade-down'><b>Rush Begins Towards the Beginning of the Spring Semester. <br></br> <a href="https://forms.gle/v8p4aJF5o6r3EZbFA">Fill out our interest form to begin the rush process!</a></b></h4>
+         <h4 className='secondaryText' data-aos='fade-down'><b>Rush Begins Saturday January 27th. <br></br> <a href="https://forms.gle/v8p4aJF5o6r3EZbFA">Fill out our interest form to begin the rush process!</a></b></h4>
          <h5>If you have Any questions feel free to reach out to one of our rush chairs: <br></br>
               Parker Homann -> 267 - 216 - 5865<br></br>
               Jackson LaFever ->  240 - 786 - 8784<br></br>
@@ -38,37 +38,37 @@ const RushPage = () => (
     
     
 
-         <h2 className='pageSubtitle' data-aos='fade-down' data-aos-delay='100'>Schedule Coming Soon</h2>
+         <h2 className='pageSubtitle' data-aos='fade-down' data-aos-delay='100'>Spring Rush Schedule</h2>
 
-      {/*<Event date={'27'} month={'Aug'} eventName={'Meet the Greeks'} location={'La Plata Beach'}>
-        <p><b> Time: 1-5 </b></p>
+      <Event date={'27'} month={'Jan'} eventName={'Meet the Greeks'} location={'Riggs Alumni Center: Orem Hall'}>
+        <p><b> Time: 3-6 </b></p>
         <p> Come meet the brothers at Meet the Greeks! </p>
       </Event>
 
-        <Event date={'29'} month={'Aug'} eventName={'Bonfire'} location={'7507 Rhode Island Avenue'} alt>
+        <Event date={'28'} month={'Jan'} eventName={'Open House'} location={'2 Fraternity Row'} alt>
+        <p><b> Time: 2-5pm </b></p>
+        <p> Swing by to see our Chapter House!  </p>
+      </Event>
+
+        <Event date={'28'} month={'Jan'} eventName={'Playoff Watch Party'} location={'2 Fraternity Row'}>
         <p><b> Time: 6pm </b></p>
-        <p>  </p>
+        <p> Stick Around to watch the playoffs, enjoy some food, and talk with brothers! </p>
       </Event>
 
-        <Event date={'4/5'} month={'Sep'} eventName={'Interviews'} location={'Invite Only'}>
-        <p><b> </b></p>
-        <p>  </p>
-</Event>*/}
-
-        {/*<Event date={'10'} month={'Feb'} eventName={'Games Night'} location={'Zoom'} alt>
-        <p><b> Time TBD </b></p>
-        <p> Complete above Google Form for the Zoom Link </p>
+        <Event date={'29'} month={'Jan'} eventName={'Mechanical Bull'} location={'7507 Princeton Ave'} alt>
+        <p><b> Time: 6-9 </b></p>
+        <p> Welcome to the Rodeo! </p>
       </Event>
 
-        <Event date={'11'} month={'Feb'} eventName={'Interviews (Invite Only)'} location={'Zoom'}>
-        <p><b> Time TBD </b></p>
-        <p> Complete above Google Form for the Zoom Link </p>
+        <Event date={'31'} month={'Jan'} eventName={'Bonfire'} location={'TBD'}>
+        <p><b> Time: 6-9 </b></p>
+        <p> Enjoy Food and Fire with the brothers! </p>
       </Event>
 
-        <Event date={'13'} month={'Feb'} eventName={'Brothers AMA'} location={'Zoom'} alt>
+        <Event date={'1'} month={'Feb'} eventName={'Invite Only'} location={'Invite Only'} alt>
         <p><b> Time TBD </b></p>
-  <p> Complete above Google Form for the Zoom Link </p>
-  </Event> */}
+        <p> </p>
+  </Event> 
 
     </Container>
 
